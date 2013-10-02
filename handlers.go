a66cf3d4396ba191041e28ee067c241ede26bd96
John Berthels <john_berthels@trendmicro.co.uk>
2013-10-02 09:21:30 +0100

    send back status as json

--- handlers.go
+++ handlers.go
@@ -21,8 +21,8 @@ func gopHandler(g *Req, w http.ResponseWriter, r *http.Request) {
     }
     vars := mux.Vars(r)
     switch vars["action"] {
-        case "request-status": {
-            handleRequestStatus(g, w, r)
+        case "status": {
+            handleStatus(g, w, r)
             return
         }
         case "mem": {
@@ -40,28 +40,66 @@ func gopHandler(g *Req, w http.ResponseWriter, r *http.Request) {
     }
 }
 
-func handleMem(g *Req, w http.ResponseWriter, r *http.Request) {
-    var memStats runtime.MemStats
-    runtime.ReadMemStats(&memStats)
-    json, err := json.Marshal(memStats)
+func sendJson(g *Req, w http.ResponseWriter, what string, v interface{}) {
+    json, err := json.Marshal(v)
     if err != nil {
-        g.Error("Failed to encode memstats as json: %s", err.Error())
-        http.Error(w, "Failed to get stats", http.StatusInternalServerError)
+        g.Error("Failed to encode %s as json: %s", what, err.Error())
+        http.Error(w, "Failed to encode json: " + err.Error(), http.StatusInternalServerError)
         return
     }
     w.Header().Set("Content-Type", "text/json")
     w.Write(json)
 }
 
-func handleRequestStatus(g *Req, w http.ResponseWriter, r *http.Request) {
+func handleMem(g *Req, w http.ResponseWriter, r *http.Request) {
+    var memStats runtime.MemStats
+    runtime.ReadMemStats(&memStats)
+    sendJson(g, w, "memstats", memStats)
+}
+
+func handleStatus(g *Req, w http.ResponseWriter, r *http.Request) {
+    type requestInfo struct {
+        Id              int
+        Method          string
+        Url             string
+        Duration        float64
+    }
+    type requestStatus struct {
+        ProjectName     string
+        AppName         string
+        Pid             int
+        StartTime       time.Time
+        UptimeSeconds   float64
+        RequestInfo     []requestInfo
+    }
+    appDuration := time.Since(g.app.startTime).Seconds()
+    status := requestStatus {
+        ProjectName: g.app.ProjectName,
+        AppName: g.app.AppName,
+        Pid: os.Getpid(),
+        StartTime: g.app.startTime,
+        UptimeSeconds: appDuration,
+    }
     reqChan := make(chan *Req)
     g.app.getReqs <- reqChan
-    appDuration := time.Since(g.app.startTime).Seconds()
+    for req := range reqChan {
+        reqDuration := time.Since(req.startTime)
+        info := requestInfo{
+            Id: req.id,
+            Method: req.r.Method,
+            Url: req.r.URL.String(),
+            Duration: reqDuration.Seconds(),
+        }
+        status.RequestInfo = append(status.RequestInfo, info)
+    }
+    sendJson(g, w, "status", status)
+    /*
     fmt.Fprintf(w, "%s - %s PID %d up for %.3fs (%s)\n\n", g.app.ProjectName, g.app.AppName, os.Getpid(), appDuration, g.app.startTime)
     for req := range reqChan {
         reqDuration := time.Since(req.startTime).Seconds()
         fmt.Fprintf(w, "%d: %.3f\t%s\t%s\n", req.id, reqDuration, req.r.Method, req.r.URL.String())
     }
+    */
 }
 
 func handleTest(g *Req, w http.ResponseWriter, r *http.Request) {
