8b91c798043f4ff22d60b08aa9bedbf87b1b6ae5
John Berthels <john@berthels.co.uk>
2013-10-08 14:26:46 +0100

    attempt to relay SIGTERM signal to child process group

--- nelly/nelly.go
+++ nelly/nelly.go
@@ -6,6 +6,7 @@ import (
     "flag"
     "fmt"
     "os"
+    "os/signal"
     "time"
     "syscall"
 )
@@ -16,10 +17,6 @@ func main() {
 
     var projectName string
     flag.StringVar(&projectName, "project", "", "Name of project")
-
-    var watchdogSecs float64
-    flag.Float64Var(&watchdogSecs, "watchdog_secs", 1, "Number of seconds between checks")
-    flag.Parse()
 
 	if projectName == "" {
 		println("You must specify the name of a projec with --project=project_name")
@@ -32,6 +29,8 @@ func main() {
 
     // We won't run gop, but load it up for config and logging
     a := gop.Init(projectName, appName) 
+
+    checkSecs, _ := a.Cfg.GetFloat32("gop", "nelly_check_secs", 1.0)
 
     a.Info("nelly initialised for [%s:%s]", projectName, appName)
 
@@ -51,17 +50,35 @@ func main() {
     // So we can send a sig of 0 to a process group and see if the process group is empty
     // empty process group => we need to restart
 
-    ticker := time.Tick(time.Second * time.Duration(watchdogSecs))
+    sigChan := setupSignals(a, proc)
+
+    pgid := proc.Pid
+
+    ticker := time.Tick(time.Second * time.Duration(checkSecs))
+LOOP:
     for {
-        // Wait at least one tick, so the child has time to change it's
-        // process group to be the same as it's pid
-        if processGroupEmpty(a, proc.Pid) {
-            a.Error("Process group empty")
-            break
+        select {
+            case <- ticker: {
+                if processGroupEmpty(a, pgid) {
+                    a.Error("Process group [%d] empty", pgid)
+                    break LOOP
+                }
+            }
+            case sig := <- sigChan: {
+                a.Error("Caught signal: %s - killing process group", sig)
+                syscall.Kill(-pgid, syscall.SIGTERM)
+                a.Error("Exiting on SIGTERM")
+                os.Exit(0)
+            }
         }
-        <- ticker
     }
     a.Error("Descendants are dead - exiting")
+}
+
+func setupSignals(a *gop.App, proc *os.Process) chan os.Signal {
+    sigChan := make(chan os.Signal, 10)     // 10 is arbitrary, we just need to keep up
+    signal.Notify(sigChan, syscall.SIGTERM)
+    return sigChan
 }
 
 func processGroupEmpty(a *gop.App, pgid int) bool {
