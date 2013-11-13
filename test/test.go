219f8bda5e789fbb5dfc3133300183a09839eac3
John Berthels <john_berthels@trendmicro.co.uk>
2013-11-13 14:22:42 +0000

    add ErrIs error checking helper

--- test/test.go
+++ test/test.go
@@ -4,10 +4,18 @@ import (
 	"testing"
 )
 
-func OK(t *testing.T, assertion bool, msg string) {
+func Assert(t *testing.T, assertion bool, goodMsg, badMsg string) {
 	if assertion {
-		t.Log("OK: ", msg)
+		t.Log("OK: ", goodMsg)
 	} else {
-		t.Error("NOT OK: ", msg)
+		t.Error("NOT OK: ", badMsg)
 	}
+}
+
+func OK(t *testing.T, assertion bool, msg string) {
+	Assert(t, assertion, msg, msg)
+}
+
+func ErrIs(t *testing.T, got, expected error, msg string) {
+	Assert(t, got == expected, msg, msg + " err: " + got.Error())
 }
