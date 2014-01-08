82a17ac39c3df067a53f273e6971a47f79dc27b1
John Berthels <john_berthels@trendmicro.co.uk>
2014-01-08 16:03:41 +0000

    move the testconfig override functionality into the base gop config

--- config.go
+++ config.go
@@ -9,18 +9,18 @@ import (
 	"strings"
 )
 
-type Config interface {
+type ConfigSource interface {
 	Get(sName, k string, def string) (string, bool)
-	GetInt(sName, k string, def int) (int, bool)
-	GetInt64(sName, k string, def int64) (int64, bool)
-	GetBool(sName, k string, def bool) (bool, bool)
-	GetFloat32(sName, k string, def float32) (float32, bool)
-	GetList(sName, k string, def []string) ([]string, bool)
 }
 
-type ConfigMap map[string]Section
+type Config struct {
+	source		ConfigSource
+	overrides 	map[string]map[string]string
+}
 
 type Section map[string]string
+type ConfigMap map[string]Section
+
 
 func (a *App) loadAppConfigFile() {
 	// We do not have logging set up yet. We just panic() on error.
@@ -43,14 +43,17 @@ func (a *App) loadAppConfigFile() {
 		panic(fmt.Sprintf("Can't load config file [%s]: %s", configFname, err.Error()))
 	}
 
-	theCfg := make(ConfigMap)
+	configSource := make(ConfigMap)
 	for section, m := range cfg {
-		theCfg[section] = make(map[string]string)
+		configSource[section] = make(map[string]string)
 		for k, v := range m {
-			theCfg[section][k] = v
+			configSource[section][k] = v
 		}
 	}
-	a.Cfg = &theCfg
+	a.Cfg = Config{
+		source:		&configSource, 
+		overrides:	make(map[string]map[string]string),
+	}
 }
 
 func (cfg *ConfigMap) Get(sName, k string, def string) (string, bool) {
@@ -65,8 +68,35 @@ func (cfg *ConfigMap) Get(sName, k string, def string) (string, bool) {
 	return v, true
 }
 
-func (cfg *ConfigMap) GetInt(sName, k string, def int) (int, bool) {
-	v, found := cfg.Get(sName, k, "")
+
+
+
+func (cfg *Config) Override(sectionName, key, val string) {
+	section, ok := cfg.overrides[sectionName]
+	if !ok {
+		cfg.overrides[sectionName] = make(map[string]string)
+		section = cfg.overrides[sectionName]
+	}
+	section[key] = val
+}
+
+func (cfg *Config) Get(sectionName, key string, def string) (string, bool) {
+	section, ok := cfg.overrides[sectionName]
+	if ok {
+		// Ooh...we have a section
+		val, ok := section[key]
+		if ok {
+			// Oh! and a key. Lets have that then
+			return val, true
+		}
+	}
+	// Not found, just punt it to the base
+	return cfg.source.Get(sectionName, key, def)
+}
+
+
+func (cfg *Config) GetInt(sName, k string, def int) (int, bool) {
+	v, found := cfg.source.Get(sName, k, "")
 	if !found {
 		return def, false
 	}
@@ -76,8 +106,8 @@ func (cfg *ConfigMap) GetInt(sName, k string, def int) (int, bool) {
 	}
 	panic(fmt.Sprintf("Non-numeric config key %s: %s [%s]", k, v, err))
 }
-func (cfg *ConfigMap) GetInt64(sName, k string, def int64) (int64, bool) {
-	v, found := cfg.Get(sName, k, "")
+func (cfg *Config) GetInt64(sName, k string, def int64) (int64, bool) {
+	v, found := cfg.source.Get(sName, k, "")
 	if !found {
 		return def, false
 	}
@@ -87,8 +117,8 @@ func (cfg *ConfigMap) GetInt64(sName, k string, def int64) (int64, bool) {
 	}
 	panic(fmt.Sprintf("Non-numeric config key %s: %s [%s]", k, v, err))
 }
-func (cfg *ConfigMap) GetBool(sName, k string, def bool) (bool, bool) {
-	v, found := cfg.Get(sName, k, "")
+func (cfg *Config) GetBool(sName, k string, def bool) (bool, bool) {
+	v, found := cfg.source.Get(sName, k, "")
 	if !found {
 		return def, false
 	}
@@ -98,8 +128,8 @@ func (cfg *ConfigMap) GetBool(sName, k string, def bool) (bool, bool) {
 	}
 	panic(fmt.Sprintf("Bad boolean config key %s: %s", k, v))
 }
-func (cfg *ConfigMap) GetFloat32(sName, k string, def float32) (float32, bool) {
-	v, found := cfg.Get(sName, k, "")
+func (cfg *Config) GetFloat32(sName, k string, def float32) (float32, bool) {
+	v, found := cfg.source.Get(sName, k, "")
 	if !found {
 		return def, false
 	}
@@ -109,8 +139,8 @@ func (cfg *ConfigMap) GetFloat32(sName, k string, def float32) (float32, bool) {
 	}
 	panic(fmt.Sprintf("Non-numeric float32 config key %s: %s [%s]", k, v, err))
 }
-func (cfg *ConfigMap) GetList(sName, k string, def []string) ([]string, bool) {
-	vStr, found := cfg.Get(sName, k, "")
+func (cfg *Config) GetList(sName, k string, def []string) ([]string, bool) {
+	vStr, found := cfg.source.Get(sName, k, "")
 	if !found {
 		return def, false
 	}
