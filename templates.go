8663b0ba7e03e5eaa0f9fb8a2ad15143cd8d76b4
John Berthels <john@berthels.co.uk>
2014-07-08 10:56:43 +0100

    start template support

---
+++ templates.go
@@ -0,0 +1,25 @@
+package gop
+
+import (
+	"html/template"
+)
+
+func (g *Req) Render(templateData interface{}, templates ...string) error {
+	templateDir, _ := g.Cfg.Get("gop", "template_dir", "./templates")
+
+	templateFilenames := make([]string, len(templates))
+	for i := range templates {
+		templateFilenames[i] = templateDir + "/" + templates[i] + ".ght"
+	}
+	tmpl, err := template.ParseFiles(templateFilenames...)
+	if err != nil {
+		return err
+	}
+
+	g.W.Header().Set("Content-Type", "text/html")
+	err = tmpl.Execute(g.W, templateData)
+	if err != nil {
+		return ServerError("Failed to execute template: " + err.Error())
+	}
+	return nil
+}
