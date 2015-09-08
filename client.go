447a4bbfe2cdfc98b190f25b0e02aba47f0eb2be
Mark Pitchless <markpitchless@gmail.com>
2015-09-08 19:13:28 +0100

    Add gopctl cmd line and a client lib.
    A gop.Client for talking to the gop HTTP API under /gop and a command line tool
    for grabbing status, stacks, setting config etc. See gopctl -h.
    
    Something thats been brewing on the side for while, has been really helpful
    tracking down probs in running silkis and admirals, so pushing.
    Still not sure on the right cmd line API for specifing the app to talk to.

---
+++ client.go
@@ -0,0 +1,279 @@
+package gop
+
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"net/url"
+	"runtime"
+	"strings"
+	"time"
+
+	"github.com/cocoonlife/timber"
+)
+
+// Status decodes the JSON status url return
+type StatusInfo struct {
+	AppName       string
+	ProjectName   string
+	Pid           int
+	StartTime     time.Time
+	UptimeSeconds float64
+	NumGoros      int
+	RequestInfo   []RequestInfo
+}
+
+func (status StatusInfo) String() string {
+	out := ""
+	out += fmt.Sprintln("AppName", "=", status.AppName)
+	out += fmt.Sprintln("ProjectName", "=", status.ProjectName)
+	out += fmt.Sprintln("Pid", "=", status.Pid)
+	out += fmt.Sprintln("StartTime", "=", status.StartTime)
+	out += fmt.Sprintln("UptimeSeconds", "=", status.UptimeSeconds)
+	out += fmt.Sprintln("NumGoros", "=", status.NumGoros)
+	out += fmt.Sprintln("NumRequests", "=", len(status.RequestInfo))
+	return out
+}
+
+// RequestInfo details of an open request
+type RequestInfo struct {
+	Id       int
+	Method   string
+	Url      string
+	Duration float64
+	RemoteIP string
+	IsHTTPS  bool
+}
+
+func (r RequestInfo) String() string {
+	s := fmt.Sprintf("[%d] ", r.Id)
+	if r.IsHTTPS {
+		s += "https "
+	} else {
+		s += "http "
+	}
+	s += fmt.Sprintf("%s %s %fs (%s)", r.Method, r.Url, r.Duration, r.RemoteIP)
+	return s
+}
+
+type MemInfo runtime.MemStats
+
+func (mem MemInfo) String() (s string) {
+	s += fmt.Sprintf("Alloc = %dbytes\n", mem.Alloc)
+	s += fmt.Sprintf("TotalAlloc = %dbytes\n", mem.TotalAlloc)
+	s += fmt.Sprintf("Sys = %d\nbytes", mem.Sys)
+	s += fmt.Sprintf("Lookups = %d\n", mem.Lookups)
+	s += fmt.Sprintf("Mallocs = %d\n", mem.Mallocs)
+	// Main allocation heap
+	s += fmt.Sprintf("Frees = %dbytes\n", mem.Frees)
+	s += fmt.Sprintf("HeapAlloc = %dbytes\n", mem.HeapAlloc)
+	s += fmt.Sprintf("HeapSys = %dbytes\n", mem.HeapSys)
+	s += fmt.Sprintf("HeapIdle = %dbytes\n", mem.HeapIdle)
+	s += fmt.Sprintf("HeapInuse = %dbytes\n", mem.HeapInuse)
+	s += fmt.Sprintf("HeapReleased = %dbytes\n", mem.HeapReleased)
+	s += fmt.Sprintf("HeapObjects = %d\n", mem.HeapObjects)
+
+	s += fmt.Sprintf("StackInuse = %dbytes\n", mem.StackInuse)
+	s += fmt.Sprintf("StackSys = %dbytes\n", mem.StackSys)
+	s += fmt.Sprintf("MSpanInuse = %dbytes\n", mem.MSpanInuse)
+	s += fmt.Sprintf("MSpanSys = %dbytes\n", mem.MSpanSys)
+	s += fmt.Sprintf("MCacheInuse = %dbytes\n", mem.MCacheInuse)
+	s += fmt.Sprintf("MCacheSys = %dbytes\n", mem.MCacheSys)
+	s += fmt.Sprintf("BuckHashSys = %dbytes\n", mem.BuckHashSys)
+	s += fmt.Sprintf("GCSys = %dbytes\n", mem.GCSys)
+	s += fmt.Sprintf("OtherSys = %dbytes\n", mem.OtherSys)
+
+	s += fmt.Sprintf("NextGC = %d\n", mem.NextGC)
+	s += fmt.Sprintf("LastGC = %d\n", mem.LastGC)
+	s += fmt.Sprintf("PauseTotalNs = %d\n", mem.PauseTotalNs)
+	s += fmt.Sprintf("NumGC = %d\n", mem.NumGC)
+	s += fmt.Sprintf("EnableGC = %t\n", mem.EnableGC)
+	s += fmt.Sprintf("DebugGC = %t\n", mem.DebugGC)
+	return s
+}
+
+// StringInfo represents a go runtime stack.
+type StackInfo struct {
+	lines []string
+	goro  []*GoroInfo
+}
+
+// ParseStackInfo produces a StackInfo struct by parsing a stack string.
+func ParseStackInfo(txt string) StackInfo {
+	stack := StackInfo{}
+	stack.lines = strings.Split(txt, "\n")
+	var cur *GoroInfo
+	for _, l := range stack.lines {
+		if strings.HasPrefix(l, "goroutine") {
+			cur = &GoroInfo{}
+			stack.goro = append(stack.goro, cur)
+		}
+		if cur != nil {
+			cur.lines = append(cur.lines, l)
+		}
+	}
+	return stack
+}
+
+func (si StackInfo) String() string {
+	return strings.Join(si.lines, "\n")
+}
+
+// Goros returns list of goroutines in the stack.
+func (si StackInfo) Goros() (gr []*GoroInfo) {
+	return si.goro
+}
+
+// GoroInfo contains the info on a single goro from a stack parsed by StackInfo.
+type GoroInfo struct {
+	lines []string
+}
+
+func (gi GoroInfo) String() string {
+	return strings.Join(gi.lines, "\n")
+}
+
+// Head returns the 1st line of the goro info.
+func (gi GoroInfo) Head() string {
+	return gi.lines[0]
+}
+
+// RoutineLines returns the all the lines after the first.
+func (gi GoroInfo) RoutineLines() []string {
+	return gi.lines[1:]
+}
+
+// Client to access the web api exposed under the gop/ url. For use by code
+// that wants to talk to your gop applications. See the gopctl commandline
+// tool, which wraps this client.
+type Client struct {
+	AppURL url.URL
+}
+
+// NewClient creates a new client for given gop app. Pass a string url pointing
+// at the application, e.g. http://localhost:2342/gop. You can drop the /gop
+// bit of the path and use a root url, /gop get added automatically.
+// You can drop http:// and it will be added for you.
+// If string starts with a : it is assumed to be a url starting with the port,
+// on localhost.
+//
+//
+//	// These all point to the same app
+//	c, err := NewClient("http://localhost:2342/gop")
+//	c, err := NewClient("http://localhost:2342")
+//	c, err := NewClient("localhost:2342")
+//	c, err := NewClient(":2342")
+//
+func NewClient(appstr string) (client *Client, err error) {
+	if !strings.HasPrefix(appstr, "http") {
+		if strings.HasPrefix(appstr, ":") { // port only given
+			appstr = "http://localhost" + appstr
+		} else {
+			appstr = "http://" + appstr
+		}
+	}
+	appstr = strings.TrimSuffix(appstr, "/")
+	if !strings.HasSuffix(appstr, "gop") {
+		appstr = appstr + "/gop"
+	}
+	u, err := url.Parse(appstr)
+	if err != nil {
+		return nil, err
+	}
+	return &Client{
+		AppURL: *u, // note copy
+	}, nil
+}
+
+// GetText gets the path from the path given relative to the app URL, returning
+// it as a string.
+func (c *Client) GetText(path string) (txt string, err error) {
+	url := c.AppURL
+	url.Path += ("/" + path)
+	if resp, err := http.Get(url.String()); err == nil {
+		defer resp.Body.Close()
+		body := []byte{}
+		if body, err = ioutil.ReadAll(resp.Body); err == nil {
+			return string(body), nil
+		} else {
+			return "", err
+		}
+		if resp.StatusCode != 200 {
+			return string(body), fmt.Errorf("%s failed: Status:%s", url, resp.Status)
+		}
+		return string(body), err
+	} else {
+		return "", err
+	}
+}
+
+// GetJSON gets the path from the path given relative to the app URL, parsing
+// it into the interface v given.
+func (c *Client) GetJSON(path string, v interface{}) (err error) {
+	url := c.AppURL
+	url.Path += ("/" + path)
+	if resp, err := http.Get(url.String()); err == nil {
+		defer resp.Body.Close()
+		timber.Debugf("GET %s\n %s", url, resp.Status)
+		if body, err := ioutil.ReadAll(resp.Body); err == nil {
+			err = json.Unmarshal(body, v)
+			return err
+		} else if resp.StatusCode != 200 {
+			return fmt.Errorf("%s failed. Status:%s", url.String(), resp.Status)
+		}
+	} else {
+		timber.Debugf("GET %s Error:%s\n", url, err.Error())
+		return err
+	}
+	return err
+}
+
+// Status reads the gop/status url.
+func (c *Client) Status() (status StatusInfo, err error) {
+	err = c.GetJSON("status", &status)
+	return status, err
+}
+
+// Stack reads the gop/stack url.
+func (c *Client) Stack() (stack StackInfo, err error) {
+	if txt, err := c.GetText("stack"); err == nil {
+		stack = ParseStackInfo(txt)
+	}
+	return stack, err
+}
+
+// Mem reads the gop/mem url.
+func (c *Client) Mem() (mem MemInfo, err error) {
+	err = c.GetJSON("mem", &mem)
+	return mem, err
+}
+
+// Cfg reads the config gop/url returning the current ConfigMap.
+func (c *Client) Cfg() (cfg ConfigMap, err error) {
+	err = c.GetJSON("config", &cfg)
+	return cfg, err
+}
+
+// SetCfg updates the given section key with a new value by doing a PUT to the
+// gop/config/{section}/{key} url.
+func (c *Client) SetCfg(section, key, val string) (resptxt string, err error) {
+	url := c.AppURL
+	url.Path += fmt.Sprintf("/config/%s/%s", section, key)
+	client := &http.Client{}
+	request, err := http.NewRequest("PUT", url.String(), strings.NewReader(val))
+	request.ContentLength = int64(len(val))
+	response, err := client.Do(request)
+	if err != nil {
+		return "", err
+	}
+	defer response.Body.Close()
+	contents, err := ioutil.ReadAll(response.Body)
+	if err != nil {
+		return "", err
+	}
+	if response.StatusCode != 200 {
+		fmt.Errorf("ERROR. Status:", response.StatusCode)
+	}
+	return string(contents), nil
+}
---
+++ gopctl/main.go
@@ -0,0 +1,257 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"os/exec"
+	"time"
+
+	"github.com/cocoonlife/gop"
+	"github.com/spf13/cobra"
+)
+
+// cmd line for gop apps, set config, view memory etc
+type GopCtl struct {
+	*cobra.Command
+	gopApp  string
+	C       *gop.Client
+	pretty  bool
+	created bool
+}
+
+type cmdFunc func(cmd *cobra.Command, args []string)
+
+// NewGopCtl creates an new GopCtl reader to Execute.
+func NewGopCtl() *GopCtl {
+	ctl := &GopCtl{}
+	ctl.Command = &cobra.Command{
+		Use:   "gopctl",
+		Short: "Command line tool to remote control a gop app.",
+		Example: `	gopctl --app=http://localhost:1732 status
+	gopctl --app=localhost:1732 mem
+	gopctl --app=:1742 stack
+
+	gopctl --app=:1732 get
+	gopctl --app=:1732 get gop
+	gopctl --app=:1732 get gop log_level
+
+	gopctl --app=:1732 set gop log_level debug`,
+		Run:              func(cmd *cobra.Command, args []string) { cmd.Help() },
+		PersistentPreRun: ctl.preRun,
+	}
+	// Persistent flags availiable to all (sub) commands.
+	ctl.PersistentFlags().StringVarP(&ctl.gopApp, "app", "", ":1732",
+		"base URL for the gop process you want to control")
+	//ctl.PersistentFlags().BoolVarP(&ctl.pretty, "pretty", "", false, "pretty print JSON etc in output")
+
+	ctl.addCmd("status", "Print the current app status.", func(cmd *cobra.Command, args []string) {
+		fmt.Print(ctl.Status())
+	})
+
+	ctl.addCmd("mem", "Print the current memory usage", func(cmd *cobra.Command, args []string) {
+		fmt.Print(ctl.Mem())
+	})
+
+	ctl.addCmd("stack", "Print the current go stack", func(cmd *cobra.Command, args []string) {
+		fmt.Print(ctl.Stack())
+	})
+
+	ctl.addCmd("get [section] [key]", "Get all config, a sections config or key value", ctl.cfgGet)
+	ctl.addCmd("set <section> <key> <value>", "Set a config keys value", ctl.cfgSet)
+
+	ctl.addCmd("requests", "Print the list of current active requests", ctl.requests)
+
+	c := ctl.addCmd("goros", "Print the list of current goroutines", ctl.goros)
+	c.Flags().BoolVar(&ctl.created, "created", false, "show creating routine for each goro.")
+
+	ctl.addCmd("top", "Top like summary", ctl.top)
+
+	c = &cobra.Command{
+		Use:     "raw PATH",
+		Short:   "Get raw responses.",
+		Example: "gopctl raw status\ngopctl --app :1742 raw mem | jq .Alloc",
+		Run: func(cmd *cobra.Command, args []string) {
+			path := args[0]
+			txt, err := ctl.C.GetText(path)
+			if err != nil {
+				log.Fatalf(err.Error())
+			}
+			fmt.Print(txt)
+		},
+	}
+	ctl.AddCommand(c)
+
+	return ctl
+}
+
+// preRun for all commands, sets up the client from options
+func (ctl *GopCtl) preRun(cmd *cobra.Command, args []string) {
+	if c, err := gop.NewClient(ctl.gopApp); err != nil {
+		log.Fatalf("Failed to create client: %s", err.Error())
+	} else {
+		ctl.C = c
+	}
+}
+
+func (ctl *GopCtl) addCmd(use, short string, run func(cmd *cobra.Command, args []string)) *cobra.Command {
+	c := &cobra.Command{
+		Use:   use,
+		Short: short,
+		Run:   run,
+	}
+	ctl.AddCommand(c)
+	return c
+}
+
+// Status wraps client Status call, logging any fail at fatal
+func (ctl *GopCtl) Status() gop.StatusInfo {
+	status, err := ctl.C.Status()
+	if err != nil {
+		log.Fatalf("Failed to get status: %s", err.Error())
+	}
+	return status
+}
+
+// Stack wraps client Stack call, logging any fail at fatal
+func (ctl *GopCtl) Stack() gop.StackInfo {
+	stack, err := ctl.C.Stack()
+	if err != nil {
+		log.Fatal("Failed to get stack: ", err.Error())
+	}
+	return stack
+}
+
+// Mem wraps client Mem call, logging any fail at fatal
+func (ctl *GopCtl) Mem() gop.MemInfo {
+	mem, err := ctl.C.Mem()
+	if err != nil {
+		log.Fatal("Failed to get mem: ", err.Error())
+	}
+	return mem
+}
+
+// Cfg wraps the client Cfg call, logging any fail at fatal
+func (ctl *GopCtl) Cfg() gop.ConfigMap {
+	cfg, err := ctl.C.Cfg()
+	if err != nil {
+		log.Fatalf("Failed to get config : %s", err.Error())
+	}
+	return cfg
+}
+
+func (c *GopCtl) cfgGet(cmd *cobra.Command, args []string) {
+	// Grab the config
+	cfg := c.Cfg()
+
+	switch len(args) {
+	case 0:
+		for _, section := range cfg.Sections() {
+			fmt.Printf("[%s]\n", section)
+			for _, key := range cfg.SectionKeys(section) {
+				if v, ok := cfg.Get(section, key, "?"); ok {
+					fmt.Printf("%s = %s\n", key, v)
+				}
+			}
+			fmt.Printf("\n")
+		}
+	case 1:
+		section := args[0]
+		for _, key := range cfg.SectionKeys(section) {
+			if v, ok := cfg.Get(section, key, "?"); ok {
+				fmt.Printf("%s = %s\n", key, v)
+			}
+		}
+	case 2:
+		section := args[0]
+		key := args[1]
+		if v, ok := cfg.Get(section, key, "?"); ok {
+			fmt.Printf("%s\n", v)
+		} else {
+			fmt.Printf("NOT SET")
+		}
+	default:
+		log.Fatal("Too many args!")
+	}
+}
+
+func (c *GopCtl) cfgSet(cmd *cobra.Command, args []string) {
+	if len(args) != 3 {
+		log.Fatal("Need section, key and value args")
+	}
+	section := args[0]
+	key := args[1]
+	val := args[2]
+	contents, err := c.C.SetCfg(section, key, val)
+	if err != nil {
+		log.Fatal("Failed to set: ", err.Error())
+	}
+	fmt.Print(string(contents))
+}
+
+func (ctl *GopCtl) requests(cmd *cobra.Command, args []string) {
+	status := ctl.Status()
+	for _, req := range status.RequestInfo {
+		fmt.Println(req)
+	}
+}
+
+func (ctl *GopCtl) goros(cmd *cobra.Command, args []string) {
+	for _, goro := range ctl.Stack().Goros() {
+		lines := goro.RoutineLines()
+		fmt.Println(goro.Head(), lines[0])
+		if ctl.created && len(lines) > 2 {
+			fmt.Println("\t", lines[len(lines)-3])
+		}
+	}
+}
+
+func (ctl *GopCtl) top(cmd *cobra.Command, args []string) {
+	for {
+		sh := exec.Command("clear") //Linux only
+		sh.Stdout = os.Stdout
+		sh.Run()
+
+		status := ctl.Status()
+		mem := ctl.Mem()
+
+		fmt.Println(status.ProjectName, status.AppName, "\t", ctl.C.AppURL.String(), "\t at", time.Now())
+		fmt.Printf("Started at %s up for %fs\n", status.StartTime, status.UptimeSeconds)
+
+		fmt.Printf("\nMEM:\t")
+		fmt.Printf("Alloc %dbytes\t", mem.Alloc)
+		fmt.Printf("Total %dbytes\t", mem.TotalAlloc)
+		fmt.Printf("Sys %dbytes\t", mem.Sys)
+		fmt.Printf("Lookups %d\t", mem.Lookups)
+		fmt.Printf("Mallocs %d\t", mem.Mallocs)
+
+		fmt.Printf("\nGC:\t")
+		fmt.Printf("%d\t", mem.NumGC)
+		fmt.Printf("Next %d\t", mem.NextGC)
+		fmt.Printf("Last %d\t", mem.LastGC)
+		fmt.Printf("PauseTotal %dns\t", mem.PauseTotalNs)
+		fmt.Printf("Enable %t\t", mem.EnableGC)
+		fmt.Printf("Debug %t\t", mem.DebugGC)
+		fmt.Println()
+
+		fmt.Println()
+		fmt.Println("Requests:", len(status.RequestInfo))
+		for _, req := range status.RequestInfo {
+			fmt.Println("\t", req)
+		}
+
+		stack := ctl.Stack()
+		fmt.Println()
+		fmt.Printf("Goros: %d\n", status.NumGoros)
+		for _, goro := range stack.Goros() {
+			fmt.Println("\t", goro.Head())
+		}
+
+		time.Sleep(2 * time.Second)
+	}
+}
+
+func main() {
+	c := NewGopCtl()
+	c.Execute()
+}
