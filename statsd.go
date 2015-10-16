0e3fddb687d43d77212366994b1d96eacbeefef0
Will Newton <will.newton@cocoon.life>
2015-10-16 16:53:13 +0100

    statsd.go: Allow stats configuration to fail
    If statsd configuration fails use a null statsd client that does
    nothing. This avoids a misconfigured or inaccessible statsd server
    causing panics in the application.

--- statsd.go
+++ statsd.go
@@ -7,10 +7,23 @@ import (
 	"github.com/cactus/go-statsd-client/statsd"
 )
 
-type StatsdClient struct {
-	client statsd.Statter
-	rate   float32
-	app    *App
+type StatsdClient interface {
+	Dec(stat string, value int64)
+	Gauge(stat string, value int64)
+	GaugeDelta(stat string, value int64)
+	Inc(stat string, value int64)
+	Timing(stat string, delta int64)
+	TimingDuration(stat string, delta time.Duration)
+	// TimingTrack records the time something took to run using a defer,
+	// very good for timing a function call, just add a the defer at the
+	// top like so:
+	//     func timeMe() {
+	//         defer app.Stats.TimingTrack("timeMe.run_time", time.Now())
+	//         // rest of code, can return anywhere and run time tracked
+	//     }
+	// This works because the time.Now() is run when defer line is reached,
+	// so gets the time timeMe started.
+	TimingTrack(stat string, start time.Time)
 }
 
 func (a *App) initStatsd() {
@@ -30,15 +43,15 @@ func (a *App) configureStatsd(cfg *Config) {
 	a.Fine("STATSD PREFIX %s", statsdPrefix)
 	client, err := statsd.New(statsdHostport, statsdPrefix)
 	if err != nil {
-		// App will probably fall over due to nil client. That's OK.
-		// We *could* panic below, but lets try and continue at least
-		a.Error("Failed to create statsd client: " + err.Error())
+		a.Error("Failed to create network statsd client: " + err.Error())
+		// Create a null client that does nothing.
+		a.Stats = NullStatsdClient{}
 		return
 	}
 
 	rate, _ := a.Cfg.GetFloat32("gop", "statsd_rate", 1.0)
 	// TODO: Need to protect a.Stats from race
-	a.Stats = StatsdClient{
+	a.Stats = NetworkStatsdClient{
 		client: client,
 		rate:   rate,
 		app:    a,
@@ -47,46 +60,67 @@ func (a *App) configureStatsd(cfg *Config) {
 	a.Finef("STATSD sending to [%s] with prefix [%s] at rate [%f]", statsdHostport, statsdPrefix, rate)
 }
 
-func (s *StatsdClient) Dec(stat string, value int64) {
+type NetworkStatsdClient struct {
+	client statsd.Statter
+	rate   float32
+	app    *App
+}
+
+func (s NetworkStatsdClient) Dec(stat string, value int64) {
 	s.app.Fine("STATSD DEC %s %d", stat, value)
 	_ = s.client.Dec(stat, value, s.rate)
 }
 
-func (s *StatsdClient) Gauge(stat string, value int64) {
+func (s NetworkStatsdClient) Gauge(stat string, value int64) {
 	s.app.Fine("STATSD GAUGE %s %d", stat, value)
 	_ = s.client.Gauge(stat, value, s.rate)
 }
 
-func (s *StatsdClient) GaugeDelta(stat string, value int64) {
+func (s NetworkStatsdClient) GaugeDelta(stat string, value int64) {
 	s.app.Fine("STATSD GAUGEDELTA %s %d", stat, value)
 	_ = s.client.GaugeDelta(stat, value, s.rate)
 }
 
-func (s *StatsdClient) Inc(stat string, value int64) {
+func (s NetworkStatsdClient) Inc(stat string, value int64) {
 	s.app.Fine("STATSD INC %s %d", stat, value)
 	_ = s.client.Inc(stat, value, s.rate)
 }
 
-func (s *StatsdClient) Timing(stat string, delta int64) {
+func (s NetworkStatsdClient) Timing(stat string, delta int64) {
 	s.app.Fine("STATSD TIMING %s %d", stat, delta)
 	_ = s.client.Timing(stat, delta, s.rate)
 }
 
-func (s *StatsdClient) TimingDuration(stat string, delta time.Duration) {
+func (s NetworkStatsdClient) TimingDuration(stat string, delta time.Duration) {
 	s.app.Fine("STATSD TIMING %s %s", stat, delta)
 	_ = s.client.TimingDuration(stat, delta, s.rate)
 }
 
-// TimingTrack records the time something took to run using a defer, very good
-// for timing a function call, just add a the defer at the top like so:
-//     func timeMe() {
-//         defer app.Stats.TimingTrack("timeMe.run_time", time.Now())
-//         // rest of code, can return anywhere and run time tracked
-//     }
-// This works because the time.Now() is run when defer line is reached, so gets
-// the time timeMe statred
-func (s *StatsdClient) TimingTrack(stat string, start time.Time) {
+func (s NetworkStatsdClient) TimingTrack(stat string, start time.Time) {
 	elapsed := time.Since(start)
 	s.app.Finef("STATSD TIMING %s %s", stat, elapsed)
 	_ = s.client.TimingDuration(stat, elapsed, s.rate)
+}
+
+type NullStatsdClient struct{}
+
+func (s NullStatsdClient) Dec(stat string, value int64) {
+}
+
+func (s NullStatsdClient) Gauge(stat string, value int64) {
+}
+
+func (s NullStatsdClient) GaugeDelta(stat string, value int64) {
+}
+
+func (s NullStatsdClient) Inc(stat string, value int64) {
+}
+
+func (s NullStatsdClient) Timing(stat string, delta int64) {
+}
+
+func (s NullStatsdClient) TimingDuration(stat string, delta time.Duration) {
+}
+
+func (s NullStatsdClient) TimingTrack(stat string, start time.Time) {
 }
